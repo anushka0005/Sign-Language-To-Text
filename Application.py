7fd5a9fdf86fc3c5cdef0037ce271ee1300b032b
Anushka Shukla <anushkashukla0510@gmail.com>
2025-05-17 18:50:22 +0530

    changes in app.py and added Hunspell dict files

--- Application.py
+++ Application.py
@@ -1,7 +1,6 @@
 # Importing Libraries
 
 import numpy as np
-
 import cv2
 import os, sys
 import time
@@ -13,7 +12,7 @@ import tkinter as tk
 from PIL import Image, ImageTk
 
 from hunspell import Hunspell
-import enchant
+#import enchant
 
 from keras.models import model_from_json
 
@@ -25,42 +24,44 @@ class Application:
 
     def __init__(self):
 
-        self.hs = Hunspell('en_US')
+        # Use specific path to dictionary files
+        self.hs = Hunspell('en_US', hunspell_data_dir='dictionaries/en_US')
         self.vs = cv2.VideoCapture(0)
         self.current_image = None
         self.current_image2 = None
-        self.json_file = open("Models\model_new.json", "r")
+        # Fix backslash path issues on Windows
+        self.json_file = open("Models/model_new.json", "r")
         self.model_json = self.json_file.read()
         self.json_file.close()
 
         self.loaded_model = model_from_json(self.model_json)
-        self.loaded_model.load_weights("Models\model_new.h5")
+        self.loaded_model.load_weights("Models/model_new.h5")
 
-        self.json_file_dru = open("Models\model-bw_dru.json" , "r")
+        self.json_file_dru = open("Models/model-bw_dru.json", "r")
         self.model_json_dru = self.json_file_dru.read()
         self.json_file_dru.close()
 
         self.loaded_model_dru = model_from_json(self.model_json_dru)
-        self.loaded_model_dru.load_weights("Models\model-bw_dru.h5")
-        self.json_file_tkdi = open("Models\model-bw_tkdi.json" , "r")
+        self.loaded_model_dru.load_weights("Models/model-bw_dru.h5")
+        self.json_file_tkdi = open("Models/model-bw_tkdi.json", "r")
         self.model_json_tkdi = self.json_file_tkdi.read()
         self.json_file_tkdi.close()
 
         self.loaded_model_tkdi = model_from_json(self.model_json_tkdi)
-        self.loaded_model_tkdi.load_weights("Models\model-bw_tkdi.h5")
-        self.json_file_smn = open("Models\model-bw_smn.json" , "r")
+        self.loaded_model_tkdi.load_weights("Models/model-bw_tkdi.h5")
+        self.json_file_smn = open("Models/model-bw_smn.json", "r")
         self.model_json_smn = self.json_file_smn.read()
         self.json_file_smn.close()
 
         self.loaded_model_smn = model_from_json(self.model_json_smn)
-        self.loaded_model_smn.load_weights("Models\model-bw_smn.h5")
+        self.loaded_model_smn.load_weights("Models/model-bw_smn.h5")
 
         self.ct = {}
         self.ct['blank'] = 0
         self.blank_flag = 0
 
         for i in ascii_uppercase:
-          self.ct[i] = 0
+            self.ct[i] = 0
         
         print("Loaded model from disk")
 
@@ -98,7 +99,7 @@ class Application:
 
         self.T3 = tk.Label(self.root)
         self.T3.place(x = 10, y = 645)
-        self.T3.config(text = "Sentence :",font = ("Courier", 30, "bold"))
+        self.T3.config(text = "Sentence :", font = ("Courier", 30, "bold"))
 
         self.T4 = tk.Label(self.root)
         self.T4.place(x = 250, y = 690)
@@ -113,13 +114,11 @@ class Application:
         self.bt3 = tk.Button(self.root, command = self.action3, height = 0, width = 0)
         self.bt3.place(x = 625, y = 745)
 
-
         self.str = ""
         self.word = " "
         self.current_symbol = "Empty"
         self.photo = "Empty"
         self.video_loop()
-
 
     def video_loop(self):
         ok, frame = self.vs.read()
@@ -132,7 +131,7 @@ class Application:
             x2 = frame.shape[1] - 10
             y2 = int(0.5 * frame.shape[1])
 
-            cv2.rectangle(frame, (x1 - 1, y1 - 1), (x2 + 1, y2 + 1), (255, 0, 0) ,1)
+            cv2.rectangle(frame, (x1 - 1, y1 - 1), (x2 + 1, y2 + 1), (255, 0, 0), 1)
             cv2image = cv2.cvtColor(cv2image, cv2.COLOR_BGR2RGBA)
 
             self.current_image = Image.fromarray(cv2image)
@@ -147,7 +146,7 @@ class Application:
 
             blur = cv2.GaussianBlur(gray, (5, 5), 2)
 
-            th3 = cv2.adaptiveThreshold(blur, 255 ,cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 11, 2)
+            th3 = cv2.adaptiveThreshold(blur, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 11, 2)
 
             ret, res = cv2.threshold(th3, 70, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
             
@@ -164,49 +163,37 @@ class Application:
 
             self.panel4.config(text = self.word, font = ("Courier", 30))
 
-            self.panel5.config(text = self.str,font = ("Courier", 30))
+            self.panel5.config(text = self.str, font = ("Courier", 30))
 
             predicts = self.hs.suggest(self.word)
             
             if(len(predicts) > 1):
-
                 self.bt1.config(text = predicts[0], font = ("Courier", 20))
-
             else:
-
                 self.bt1.config(text = "")
 
             if(len(predicts) > 2):
-
                 self.bt2.config(text = predicts[1], font = ("Courier", 20))
-
             else:
-
                 self.bt2.config(text = "")
 
             if(len(predicts) > 3):
-
                 self.bt3.config(text = predicts[2], font = ("Courier", 20))
-
             else:
-
                 self.bt3.config(text = "")
-
 
         self.root.after(5, self.video_loop)
 
     def predict(self, test_image):
-
         test_image = cv2.resize(test_image, (128, 128))
 
         result = self.loaded_model.predict(test_image.reshape(1, 128, 128, 1))
 
+        result_dru = self.loaded_model_dru.predict(test_image.reshape(1, 128, 128, 1))
 
-        result_dru = self.loaded_model_dru.predict(test_image.reshape(1 , 128 , 128 , 1))
+        result_tkdi = self.loaded_model_tkdi.predict(test_image.reshape(1, 128, 128, 1))
 
-        result_tkdi = self.loaded_model_tkdi.predict(test_image.reshape(1 , 128 , 128 , 1))
-
-        result_smn = self.loaded_model_smn.predict(test_image.reshape(1 , 128 , 128 , 1))
+        result_smn = self.loaded_model_smn.predict(test_image.reshape(1, 128, 128, 1))
 
         prediction = {}
 
@@ -215,72 +202,53 @@ class Application:
         inde = 1
 
         for i in ascii_uppercase:
-
             prediction[i] = result[0][inde]
-
             inde += 1
 
-        #LAYER 1
-
+        # LAYER 1
         prediction = sorted(prediction.items(), key = operator.itemgetter(1), reverse = True)
-
         self.current_symbol = prediction[0][0]
 
-
-        #LAYER 2
-
+        # LAYER 2
         if(self.current_symbol == 'D' or self.current_symbol == 'R' or self.current_symbol == 'U'):
+            prediction = {}
+            prediction['D'] = result_dru[0][0]
+            prediction['R'] = result_dru[0][1]
+            prediction['U'] = result_dru[0][2]
 
-        	prediction = {}
-
-        	prediction['D'] = result_dru[0][0]
-        	prediction['R'] = result_dru[0][1]
-        	prediction['U'] = result_dru[0][2]
-
-        	prediction = sorted(prediction.items(), key = operator.itemgetter(1), reverse = True)
-
-        	self.current_symbol = prediction[0][0]
+            prediction = sorted(prediction.items(), key = operator.itemgetter(1), reverse = True)
+            self.current_symbol = prediction[0][0]
 
         if(self.current_symbol == 'D' or self.current_symbol == 'I' or self.current_symbol == 'K' or self.current_symbol == 'T'):
+            prediction = {}
+            prediction['D'] = result_tkdi[0][0]
+            prediction['I'] = result_tkdi[0][1]
+            prediction['K'] = result_tkdi[0][2]
+            prediction['T'] = result_tkdi[0][3]
 
-        	prediction = {}
-
-        	prediction['D'] = result_tkdi[0][0]
-        	prediction['I'] = result_tkdi[0][1]
-        	prediction['K'] = result_tkdi[0][2]
-        	prediction['T'] = result_tkdi[0][3]
-
-        	prediction = sorted(prediction.items(), key = operator.itemgetter(1), reverse = True)
-
-        	self.current_symbol = prediction[0][0]
+            prediction = sorted(prediction.items(), key = operator.itemgetter(1), reverse = True)
+            self.current_symbol = prediction[0][0]
 
         if(self.current_symbol == 'M' or self.current_symbol == 'N' or self.current_symbol == 'S'):
+            prediction1 = {}
+            prediction1['M'] = result_smn[0][0]
+            prediction1['N'] = result_smn[0][1]
+            prediction1['S'] = result_smn[0][2]
 
-        	prediction1 = {}
+            prediction1 = sorted(prediction1.items(), key = operator.itemgetter(1), reverse = True)
 
-        	prediction1['M'] = result_smn[0][0]
-        	prediction1['N'] = result_smn[0][1]
-        	prediction1['S'] = result_smn[0][2]
-
-        	prediction1 = sorted(prediction1.items(), key = operator.itemgetter(1), reverse = True)
-
-        	if(prediction1[0][0] == 'S'):
-
-        		self.current_symbol = prediction1[0][0]
-
-        	else:
-
-        		self.current_symbol = prediction[0][0]
+            if(prediction1[0][0] == 'S'):
+                self.current_symbol = prediction1[0][0]
+            else:
+                self.current_symbol = prediction[0][0]
         
         if(self.current_symbol == 'blank'):
-
             for i in ascii_uppercase:
                 self.ct[i] = 0
 
         self.ct[self.current_symbol] += 1
 
         if(self.ct[self.current_symbol] > 60):
-
             for i in ascii_uppercase:
                 if i == self.current_symbol:
                     continue
@@ -303,7 +271,6 @@ class Application:
                 self.ct[i] = 0
 
             if self.current_symbol == 'blank':
-
                 if self.blank_flag == 0:
                     self.blank_flag = 1
 
@@ -311,70 +278,51 @@ class Application:
                         self.str += " "
 
                     self.str += self.word
-
                     self.word = ""
-
             else:
-
                 if(len(self.str) > 16):
                     self.str = ""
 
                 self.blank_flag = 0
-
                 self.word += self.current_symbol
 
     def action1(self):
-
-    	predicts = self.hs.suggest(self.word)
-
-    	if(len(predicts) > 0):
-
+        predicts = self.hs.suggest(self.word)
+        if(len(predicts) > 0):
             self.word = ""
-
             self.str += " "
-
             self.str += predicts[0]
 
     def action2(self):
-
-    	predicts = self.hs.suggest(self.word)
-
-    	if(len(predicts) > 1):
+        predicts = self.hs.suggest(self.word)
+        if(len(predicts) > 1):
             self.word = ""
             self.str += " "
             self.str += predicts[1]
 
     def action3(self):
-
-    	predicts = self.hs.suggest(self.word)
-
-    	if(len(predicts) > 2):
+        predicts = self.hs.suggest(self.word)
+        if(len(predicts) > 2):
             self.word = ""
             self.str += " "
             self.str += predicts[2]
 
     def action4(self):
-
-    	predicts = self.hs.suggest(self.word)
-
-    	if(len(predicts) > 3):
+        predicts = self.hs.suggest(self.word)
+        if(len(predicts) > 3):
             self.word = ""
             self.str += " "
             self.str += predicts[3]
 
     def action5(self):
-
-    	predicts = self.hs.suggest(self.word)
-
-    	if(len(predicts) > 4):
+        predicts = self.hs.suggest(self.word)
+        if(len(predicts) > 4):
             self.word = ""
             self.str += " "
             self.str += predicts[4]
             
     def destructor(self):
-
         print("Closing Application...")
-
         self.root.destroy()
         self.vs.release()
         cv2.destroyAllWindows()
